edd0f31ad8909500448b2b84297a2a389751c4fe
DHARANIDHARAN SENTHILKUMAR <99940501+DHARANI2D@users.noreply.github.com>
2023-08-09 10:49:24 +0530

    Updated logic

--- predict_grades_and_cgpa.py
+++ predict_grades_and_cgpa.py
@@ -1,5 +1,7 @@
 import streamlit as st
+import math
 
+# Define the custom grade points
 grade_points = {
     "O": 10.0,
     "A+": 9.0,
@@ -9,78 +11,68 @@ grade_points = {
     "C": 5.0,
 }
 
-def calculate_adjusted_grade(difficulty, target_cgpa):
-    if difficulty == "Difficult":
-        return grade_points["C"]
-    elif difficulty == "Moderate" and target_cgpa is not None:
-        return target_cgpa * 0.75
-    else:
-        return grade_points["A"]
+def calculate_required_grades(target_cgpa, current_cgpa, subjects):
+    total_credits = 0
+    weighted_grade_points = 0
+
+    for subject, (credits, difficulty) in subjects.items():
+        total_credits += credits
+        weighted_grade_points += credits * grade_points.get("A", 0) * (1 + difficulty)  # Use default grade as "A"
+
+    max_possible_cgpa = (current_cgpa * total_credits + weighted_grade_points) / total_credits
+    target_cgpa = min(target_cgpa, 9.5)  # Cap target CGPA at 9.5
+    
+    dp = [0] * int(max_possible_cgpa * 100 + 1)  # DP table
+
+    for grade, points in grade_points.items():
+        for i in range(int(max_possible_cgpa * 100) - int(points * 100) + 1):
+            dp[i + int(points * 100)] = max(dp[i + int(points * 100)], dp[i] + points)
+    
+    required_grades = {}
+    for subject, (credits, difficulty) in subjects.items():
+        required_grade_points = ((target_cgpa * total_credits) - weighted_grade_points + credits * dp[int(target_cgpa * 100) - int(current_cgpa * 100) + int(grade_points.get("A", 0) * 100)] * (1 + difficulty)) / (total_credits * target_cgpa)
+        required_grade_points = math.ceil(required_grade_points)  # Round up to the nearest whole number
+        required_grade = next((g for g, points in grade_points.items() if required_grade_points >= points), "C")  # Default to "C" if no grade found
+        required_grades[subject] = required_grade
+    
+    return required_grades, target_cgpa
 
 def main():
-    st.title("Grades Predictor")
+    st.title("CGPA Predictor App")
+    
+    st.write("Enter your current CGPA, target CGPA, and details of subjects:")
 
-    current_cgpa = st.number_input("Current CGPA:", min_value=0.0, max_value=9.9, step=0.1)
-    target_cgpa = st.number_input("Target CGPA:", min_value=8.0, max_value=9.9, step=0.1)
+    current_cgpa = st.number_input("Current CGPA:", min_value=0.0, max_value=10.0, step=0.01)
+    target_cgpa = st.number_input("Target CGPA:", min_value=0.0, max_value=10.0, step=0.01)
 
-    num_subjects = st.number_input("Number of Subjects:", min_value=1, value=1, step=1)
+    num_subjects = st.number_input("Number of subjects:", min_value=1, max_value=10, step=1)
 
-    subject_data = []
+    subjects = {}
     for i in range(num_subjects):
-        st.markdown(f"Subject {i + 1}")
-        subject_name = st.text_input(f"Subject {i + 1} Name:")
-        credits = st.number_input(f"Credits for {subject_name}:", key=f"credits_{i}", min_value=1.0, step=0.5)
-        difficulty = st.selectbox(
-            f"Difficulty Level for {subject_name}:",
-            ["Easy", "Moderate", "Difficult"],
-            key=f"difficulty_{i}"
-        )
-
-        adjusted_grade = calculate_adjusted_grade(difficulty, target_cgpa)
-        rounded_grade_key = max(grade_points, key=lambda key: grade_points[key] <= adjusted_grade)
-        subject_data.append((subject_name, credits, rounded_grade_key))
-
+        st.write(f"Subject {i+1}")
+        subject_name = st.text_input(f"Subject Name {i+1}:")
+        credits = st.number_input(f"Credits for {subject_name}:", min_value=1, max_value=5, step=1)
+        difficulty = st.slider(f"Difficulty Level for {subject_name}:", min_value=1, max_value=5, step=1)
+        subjects[subject_name] = (credits, difficulty)
 
     if st.button("Calculate"):
-        st.write("Estimated Grades for Each Subject:")
-        for subject_name, credits, grade_key in subject_data:
-            st.write(f"{subject_name}: {grade_key} (Credits: {credits})")
-
-        total_credits = sum(credits for _, credits, _ in subject_data)
-        weighted_sum = sum(credits * grade_points[grade_key] for _, credits, grade_key in subject_data)
-        new_cgpa = (weighted_sum + (current_cgpa * total_credits)) / (total_credits + credits)
-
+        required_grades, new_cgpa = calculate_required_grades(target_cgpa, current_cgpa, subjects)
+        st.write("Predicted grades for each subject to achieve the target CGPA:")
+        for subject, required_grade in required_grades.items():
+            st.write(f"{subject}: {required_grade}")
+        
+        
         if new_cgpa > target_cgpa:
-            cgpa_decrease = new_cgpa - target_cgpa
-            st.warning(f"Estimated CGPA exceeds Target CGPA by {cgpa_decrease:.2f}. Adjusting grades...")
+            st.write("You have underestimated yourself!")
+            st.write(f"Calculated CGPA for your level: {new_cgpa:.2f}")
+        else:
+            st.write(f"Calculated CGPA with allotted grades: {new_cgpa:.2f}")
 
-            st.write("Adjusted Grades:")
-            for i in range(num_subjects):
-                subject_data[i] = (subject_data[i][0], subject_data[i][1], "C")
-
-            for subject_name, credits, grade_key in subject_data:
-                st.write(f"{subject_name}: {grade_key} (Credits: {credits})")
-
-            new_cgpa = target_cgpa
-
-        if new_cgpa < 5.0:
-            st.warning("Estimated CGPA went below 5.0. Adjusting grades and target CGPA...")
-            reduction_factor = (5.0 - current_cgpa * total_credits) / credits
-            for i in range(num_subjects):
-                subject_data[i] = (subject_data[i][0], subject_data[i][1], calculate_adjusted_grade(subject_data[i][2], target_cgpa))
-            target_cgpa = 5.0
-            new_cgpa = 5.0
-
-        st.success(f"Estimated CGPA: {new_cgpa:.2f}")
-
-        if new_cgpa < target_cgpa:
-            st.info("Your capabilities are low estimated,So your Modified Target CGPA: {target_cgpa:.2f}")
-    
     st.sidebar.title("Similar Applications")
     st.sidebar.markdown("[CGPA Calculator](https://cgpacalculator-2d.streamlit.app/)")
     st.sidebar.markdown("[Grades Predictor](https://gradepredictor.streamlit.app/)")
 
     st.write("<p style='text-align: center;'>Crafted with fervor by 2D🤓</p>", unsafe_allow_html=True)
-
+    
 if __name__ == "__main__":
     main()
